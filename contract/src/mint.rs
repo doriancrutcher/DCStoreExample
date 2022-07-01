a0ab55f5e689139258225b3f1ca1ff0dfb92c521
Dorian Kinoo Crutcher <doriankinoocrutcher@gmail.com>
2022-07-01 16:56:10 -0700

    fixed contract nft issue

--- contract/src/mint.rs
+++ contract/src/mint.rs
@@ -76,7 +76,7 @@ impl Contract {
         let required_storage_in_bytes = env::storage_usage() - initial_storage_usage;
 
         //refund any excess storage if the user attached too much. Panic if they didn't attach enough to cover the required.
-        refund_deposit(required_storage_in_bytes);
+        // refund_deposit(required_storage_in_bytes);
     }
     pub fn check_token(&self, id:TokenId)->bool{
         self.tokens_by_id.contains_key(&id)
